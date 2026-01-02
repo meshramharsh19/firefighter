c93fda5d4196817779262ad3042834ee47f117df
meshram-harsh <hmeshram112@gmail.com>
2026-01-02 15:07:26 +0530

    BACKEND -UPDATE

--- backend/controllers/update_user.php
+++ backend/controllers/update_user.php
@@ -1,8 +1,16 @@
 <?php
 header("Access-Control-Allow-Origin: http://localhost:5173");
-header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Access-Control-Allow-Credentials: true");
+header("Access-Control-Allow-Methods: POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
 header("Content-Type: application/json");
+
+/* 🔴 VERY IMPORTANT: Preflight handle */
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+  http_response_code(200);
+  exit();
+}
+
 
 require_once("../config/db.php");
 
