e86e9b12c46367c94196e14498731d75dee730e1
meshram-harsh <hmeshram112@gmail.com>
2026-01-08 11:39:31 +0530

    docker-img generated - successfully except userroles path

--- frontend/src/components/fire-fighter/confirm-forward/ConfirmForward.jsx
+++ frontend/src/components/fire-fighter/confirm-forward/ConfirmForward.jsx
@@ -15,15 +15,13 @@ import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
 
 const API_BASE = import.meta.env.VITE_API_BASE_URL;
 
-// const API_BASE =
-//   "http://localhost/fire-fighter-new/backend/controllers";
 
 export default function ConfirmForwardIncidence() {
   const { incidentId, stationName } = useParams();
   const navigate = useNavigate();
 
   const handleFinalConfirm = async () => {
-    await fetch(`${API_BASE_BASE}/incidents/forward_incident.php`, {
+    await fetch(`${API_BASE}/incidents/forward_incident.php`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
